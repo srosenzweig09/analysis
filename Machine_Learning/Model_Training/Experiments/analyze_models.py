7c082b1e47a3b8777b3db163335c9f5776a15e13
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2021-02-03 18:17:11 +0100

    Removed extraneous calls and modified so model analyzer saves history and predictions. Plots now display number of hidden layers, number of runs, and max, min, and average peak classification predictions for Class  1.

--- Machine_Learning/Model_Training/Experiments/analyze_models.py
+++ Machine_Learning/Model_Training/Experiments/analyze_models.py
@@ -1,35 +1,34 @@
-
-# open model
-from keras.models import model_from_json
-from pandas import read_json
-from pickle import load
+try:
+    from keras.models import model_from_json
+except ModuleNotFoundError:
+    print("You must activate the conda environment! (Use alias 'csixb')")
 import numpy as np
-from tensorflow import compat
-compat.v1.logging.set_verbosity(compat.v1.logging.ERROR)
 
-
-# extract metrics
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc
-
-# plot metrics
 import matplotlib.pyplot as plt 
 # plt.style.use('../../Config/plots.mplstyle')
 
-N = 3
+from os import environ, path
+environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TF CPU warnings
 
-filepath = f'hidden_layers/hidden_layers_{4}/'
-scaler = load(open(filepath + 'scaler.pkl', 'rb'))
+# custom libraries and modules
+from logger import info, error
 
-# fig1, axs1 = plt.subplots(nrows=6, ncols=5, figsize=(20,20))
-# fig2, axs2 = plt.subplots(nrows=6, ncols=5, figsize=(20,20))
+print()
+info("Beginning model analysis.\n")
 
-auc_scores = np.array(())
+# User parameters
+num_hidden = 2
+num_models_to_evaluate = 30
+
+filepath = f'num_hidden/num_hidden_{num_hidden}/'
+
+info(f"Evaluating {num_models_to_evaluate} models with {num_hidden} hidden layers from location {filepath}")
+
 predictions = np.array(())
 
-for i in np.arange(1,31):
+for i in np.arange(1, num_models_to_evaluate + 1):
 
-    # if i == 18 or i == 20: continue
+    info(f"Evaluating model {filepath + 'model_' + str(i) + '.json'}")
 
     # load json and create model
     model_json_file = open(filepath + 'model_' + str(i) + '.json', 'r')
@@ -37,15 +36,8 @@ for i in np.arange(1,31):
     model_json_file.close()
     model = model_from_json(model_json)
 
-    history_json_file = open(filepath + 'history_' + str(i) + '.json', 'r')
-    history = read_json(history_json_file)
-    history_json_file.close()
-    print(history.keys)
-
     # load weights into new model
     model.load_weights(filepath + 'model_' + str(i) + '.h5')
-
-    print(f"{i}: [INFO] Loaded model and scaler from disk")
 
     # Load inputs
     inputs = np.load(filepath + 'test_set_' + str(i) + '.npz')
@@ -55,64 +47,33 @@ for i in np.arange(1,31):
     X = inputs['X_test']
     m = inputs['mjj_test']
 
-    # training_accuracy = history['accuracy']
-    # validation_accuracy = history['val_accuracy']
-
-    # num_epochs = len(training_accuracy)
-    # epoch_count = range(1, num_epochs + 1)
-    # if num_epochs <= 65: 
-    #     tick_max = num_epochs + (num_epochs % 5)
-    #     epoch_ticks = np.arange(0, tick_max + 1, 5)
-    # else: 
-    #     tick_max = num_epochs + (num_epochs % 10)
-    #     epoch_ticks = np.arange(0, tick_max + 1, 10)
-
-    # ax = axs1[(i-1)//5, (i-1)%5] 
-    # ax.plot(epoch_count, training_accuracy, 'r--')
-    # ax.plot(epoch_count, validation_accuracy, 'b-')
-    # ax.legend(['Training Accuracy', 'Validation Accuracy'])
-    # ax.set_xlabel('Epoch')
-    # ax.set_ylabel('Accuracy')
-    # ax.set_ylim(0.55,1.00)
-    # ax.set_xticks(epoch_ticks)
-    # plt.tight_layout()
-
-    # training_loss = history['loss']
-    # validation_loss = history['val_loss']
-
-    # ax = axs2[(i-1)//5, (i-1)%5] 
-    # ax.plot(epoch_count, training_loss, 'r--')
-    # ax.plot(epoch_count, validation_loss, 'b-')
-    # ax.legend(['Training Loss', 'Validation Loss'])
-    # ax.set_xlabel('Epoch')
-    # ax.set_ylabel('Loss')
-    # ax.set_xticks(epoch_ticks)
-    # plt.tight_layout()
-
     pred = model.predict(x)
     predictions = np.append(predictions, pred)
 
-    fpr, tpr, thresholds_keras = roc_curve(y, pred)
-    nn_auc = auc(fpr, tpr)
 
-    auc_scores = np.append(auc_scores, nn_auc)
+predictions = predictions.reshape(num_models_to_evaluate, len(x))
 
-# fig3, ax = plt.subplots()
-# ax.hist(auc_scores, bins=20, histtype='step', align='mid')
-# ax.set_xlabel('AUC')
-# ax.set_ylabel('Count')
-# fig3.savefig('auc_dist.pdf', bbox_inches='tight')
+pred_save = f"evaluation/hidden_layers_{num_hidden}_predictions.npz"
+info(f"Saving .npz file of predictions to {pred_save}")
+np.savez(pred_save, predictions=predictions)
 
-# fig1.savefig(f'evaluation/hidden_layers_{N}_repeat_acc.pdf', bbox_inches='tight')
-# fig2.savefig(f'evaluation/hidden_layers_{N}_repeat_loss.pdf', bbox_inches='tight')
+fig, ax = plt.subplots()
 
-# print(f'auc_scores = \n {auc_scores}\n')
-# print(f'Average = {np.average(auc_scores)*100:.1f}%')
+high_peak = np.array(())
+for arr in predictions:
+    n, edges, _ = ax.hist(arr, bins=100, histtype='step', align='mid')
+    pos_of_peak = np.argmax(n[10:]) + 10 # Need to skip the peak at 0, which is higher than the peak near 1. 10 was arbitrarily chosen.
+    high_peak = np.append(high_peak, (edges[pos_of_peak] + edges[pos_of_peak + 1]) / 2)
 
-predictions = predictions.shape(30,len(x))
+print(f"Maximum: {np.max(high_peak):.3f}")
+print(f"Minimum: {np.min(high_peak):.3f}")
+print(f"Average: {np.average(high_peak):.3f}")
 
-fig4, ax = plt.subplots()
-ax.hist(predictions, bins=20, histtype='step', align='mid')
 ax.set_xlabel('Classification Score')
 ax.set_ylabel('Count')
-fig3.savefig('evaluation/hidden_layers_4_score_dist.pdf', bbox_inches='tight')
+ax.text(0.3, 0.9, f"Number of Hidden Layers: {num_hidden}\nMax: {np.max(high_peak):.3f}, Min: {np.min(high_peak):.3f}, Avg: {np.average(high_peak):.3f}", transform=ax.transAxes)
+scores = f'evaluation/hidden_layers_{num_hidden}_score_dist.pdf'
+
+info(f"Saving histogram of classification score distributions to {scores}")
+fig.savefig(scores, bbox_inches='tight')
+
