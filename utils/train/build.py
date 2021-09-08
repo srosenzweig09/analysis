27959a9045c6684c85f394adac499aa93c7d0672
Suzanne <srosenzweig09@gmail.com>
2021-09-08 08:54:04 -0400

    Added LBN training class.

--- utils/train/build.py
+++ utils/train/build.py
@@ -1,4 +1,50 @@
 from . import *
+from lbn import LBN
+
+class TrainLBN():
+
+    def get_lbn_features(self, ncombos, mask):
+        jet_pt = training.get_t6('jet_pt')
+        jet_eta = training.get_t6('jet_eta')
+        jet_phi = training.get_t6('jet_phi')
+        jet_m = training.get_t6('jet_m')
+        jet_btag = training.get_t6('jet_btag')
+
+        pt_list  = ak.unzip(ak.combinations(jet_pt[mask], ncombos))
+        eta_list = ak.unzip(ak.combinations(jet_eta[mask], ncombos))
+        phi_list = ak.unzip(ak.combinations(jet_phi[mask], ncombos))
+        m_list   = ak.unzip(ak.combinations(jet_m[mask], ncombos))
+        btag = jet_btag[mask].to_numpy()
+
+        pt_list = [ak.flatten(pt) for pt in pt_list]
+        eta_list = [ak.flatten(eta) for pt in eta_list]
+        phi_list = [ak.flatten(phi) for pt in phi_list]
+        m_list = [ak.flatten(m) for pt in m_list]
+
+        p4_list = [self.p4(pt,eta,phi,m) for pt,eta,phi,m in zip(pt_list,eta_list,phi_list,m_list)]
+        input = [[p4.E, p4.px, p4.py, p4.pz] for p4 in p4_list]
+        input = np.asarray(input)
+
+        lbn_features = lbn(np.transpose(input, (2,0,1)), features=["E","px","py","pz"])
+        lbn_features = tf.concat([lbn_features, btag], axis=1)
+
+    def __init__(self, filename, n_particles=11, n_restframes=5, boost_mode=LBN.PRODUCT):
+        lbn = LBN(n_particles, n_restframes, boost_mode=boost_mode)
+        training = TrainSix(filename)
+        signal = training.correct_mask
+        cbkgd  = training.incorrect_mask
+        
+        signal_lbn_features = self.get_lbn_features(6, signal)
+        bkgd_lbn_features = self.get_lbn_features(6, cbkgd)
+    
+        features = tf.concat([signal_lbn_features, bkgd_lbn_features], axis=0)
+        targets = np.concatenate((
+            np.tile([1,0], (signal_lbn_features.get_shape()[0], 1)),
+            np.tile([0,1], (bkgd_lbn_features.get_shape()[0], 1))
+        ))
+
+    def p4(self, pt, eta, phi, m):
+        return vector.obj(pt=pt, eta=eta, phi=phi, m=m)
 
 class TrainSix():
 
@@ -34,6 +80,8 @@ class TrainSix():
         jet_phi = self.get_t6('jet_phi')
         jet_m = self.get_t6('jet_m')
         jet_btag = self.get_t6('jet_btag')
+
+        # signal_kinematics = 
 
     def get_t6(self, branch):
         return getattr(self.tree, branch)[:, :6]
