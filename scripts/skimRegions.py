fb9671d128adf87f13d803ca5837194b5b773711
Suzanne <srosenzweig09@gmail.com>
2022-03-10 08:58:02 -0500

    Cleaner look, removed extraneous library imports, added testing flag, working on looping over all signal in eos dir

--- scripts/skimRegions.py
+++ scripts/skimRegions.py
@@ -1,20 +1,17 @@
-print()
-print("[INFO] .. starting program")
+print("---------------------------------")
+print("STARTING PROGRAM")
+print("---------------------------------")
 
 from argparse import ArgumentParser
 from array import array
-import awkward as ak
 from configparser import ConfigParser
-from hep_ml import reweight
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import re
 import ROOT
-from scipy.stats import kstest
-import subprocess
 import sys
 from utils.analysis import Signal
+from utils.fileUtils import mx_my_masses
 from utils.plotter import Hist
 
 ### ------------------------------------------------------------------------------------
@@ -26,8 +23,10 @@ parser = ArgumentParser(description='Command line parser of model options and ta
 
 parser.add_argument('--cfg', dest='cfg', help='config file', required=True)
 
-parser.add_argument('--rectangular', dest='rectangular', help='', action='store_true', default=False)
+parser.add_argument('--rectangular', dest='rectangular', help='', action='store_true', default=True)
 parser.add_argument('--spherical', dest='spherical', help='', action='store_true', default=False)
+
+parser.add_argument('--testing', dest='testing', action='store_true', default=False)
 
 parser.add_argument('--dHHH', dest='dHHH', help='', action='store_true', default=False)
 parser.add_argument('--mH', dest='mH', help='', action='store_true', default=False)
@@ -42,6 +41,8 @@ parser.add_argument('--randomState', dest='rand')
 
 parser.add_argument('--rInner', dest='rInner')
 parser.add_argument('--rOuter', dest='rOuter')
+
+parser.add_argument('--all-signal', dest='allSignal', action='store_true', default=False)
 
 args = parser.parse_args()
 
@@ -67,6 +68,7 @@ config = ConfigParser()
 config.optionxform = str
 config.read(args.cfg)
 
+print()
 overwrite = False
 for hyper in BDT_dict:
     if BDT_dict[hyper] is not None:
@@ -76,6 +78,7 @@ for hyper in sphere_dict:
     if sphere_dict[hyper] is not None:
         config['spherical'][hyper] = sphere_dict[hyper]
         print(f"Overwriting [{hyper}={sphere_dict[hyper]}] from config file with command line argument.")
+print()
 
 base = config['file']['base']
 signal = config['file']['signal']
@@ -89,48 +92,13 @@ maxMX = int(config['plot']['maxMX'])
 nbins = int(config['plot']['nbins'])
 mBins = np.linspace(minMX,maxMX,nbins)
 
+if args.spherical and args.rectangular: args.rectangular = False
 if args.rectangular: region_type = 'rect'
 elif args.spherical: region_type = 'sphere'
 
 if args.dHHH: pairing = 'dHHH_pairs'
 elif args.mH: pairing = 'mH_pairs'
 pairing_type = pairing.split('_')[0]
-
-indir = f"root://cmseos.fnal.gov/{base}/{pairing}/"
-
-sigFileName = f"{indir}NMSSM/{signal}"
-sigTree = Signal(sigFileName)
-
-datFileName = f"{indir}{data}"
-datTree = Signal(datFileName)
-
-# Assumptions here:
-# Signal file is saved in an NMSSM folder with the following format:
-# /NMSSM/NMSSM_XYH_YToHH_6b_MX_700_MY_400
-# Most importantly, we are looking for the NMSSM_XYH string and the /ntuple.root string
-start = re.search('NMSSM_XYH',signal).start()
-end = re.search('/ntuple.root',signal).start()
-outputFile = f"{signal[start:end]}_{pairing_type}_{region_type}"
-
-if args.rectangular:
-    print("RECTANGULAR")
-    sigTree.rectangular_region(config)
-    datTree.rectangular_region(config)
-    sig_mX_SRhs = sigTree.np('X_m')[sigTree.SRhs_mask]
-    dat_mX_VRls = datTree.np('X_m')[datTree.VRls_mask]
-    dat_mX_VRhs = datTree.X_m[datTree.VRhs_mask]
-    dat_mX_SRls = datTree.np('X_m')[datTree.SRls_mask]
-    dat_mX_blinded = datTree.np('X_m')[datTree.blinded_mask]
-elif args.spherical:
-    print("SPHERICAL")
-    sigTree.spherical_region(config)
-    datTree.spherical_region(config)
-    sig_mX_SRhs = sigTree.np('X_m')[sigTree.A_SRhs_mask]
-    dat_mX_VRls = datTree.np('X_m')[datTree.V_SRls_mask]
-    dat_mX_VRhs = datTree.np('X_m')[datTree.V_SRhs_mask]
-    dat_mX_SRls = datTree.np('X_m')[datTree.A_SRls_mask]
-else:
-    raise AttributeError("No mass region definition!")
 
 score = float(config['score']['threshold'])
 
@@ -144,21 +112,68 @@ randomState  = int(config['BDT']['randomState'])
 
 variables    = config['BDT']['variables'].split(", ")
 
+outputFile = f"CombineInput_{pairing_type}_{region_type}"
+
 ### ------------------------------------------------------------------------------------
-## train bdt and predict weights
+## Obtain MC signal counts
+
+def get_sig_SRhs(sigFileName):
+    sigTree = Signal(sigFileName)
+    if args.rectangular:
+        sigTree.rectangular_region(config)
+        sig_mX_SRhs = sigTree.np('X_m')[sigTree.SRhs_mask]
+    elif args.spherical:
+        sigTree.spherical_region(config)
+        sig_mX_SRhs = sigTree.np('X_m')[sigTree.A_SRhs_mask]
+    n_sig_SRhs, _ = np.histogram(sig_mX_SRhs, bins=mBins)
+    n_sig_SRhs = n_sig_SRhs * sigTree.scale
+    return n_sig_SRhs
+
+indir = f"root://cmseos.fnal.gov/{base}/{pairing}/"
+signal_nSR = []
+if args.allSignal:
+    for mx,my in mx_my_masses:
+        signal = f'NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}'
+        sigFileName = f"{indir}NMSSM/{signal}/ntuple.root"
+        n_sig_SRhs = get_sig_SRhs(sigFileName)
+        signal_nSR.append(n_sig_SRhs)
+else:
+    sigFileName = f"{indir}NMSSM/{signal}"
+    n_sig_SRhs = get_sig_SRhs(sigFileName)
+    print(signal)
+    sys.exit()
+    mx_my_masses = []
+    signal_nSR.append(n_sig_SRhs)
+
+### ------------------------------------------------------------------------------------
+## Obtain data regions
+
+datFileName = f"{indir}{data}"
+datTree = Signal(datFileName)
+
+if args.rectangular:
+    print("\nRECTANGULAR")
+    datTree.rectangular_region(config)
+    dat_mX_VRls = datTree.np('X_m')[datTree.VRls_mask]
+    dat_mX_VRhs = datTree.X_m[datTree.VRhs_mask]
+    dat_mX_SRls = datTree.np('X_m')[datTree.SRls_mask]
+    dat_mX_blinded = datTree.np('X_m')[datTree.blinded_mask]
+elif args.spherical:
+    print("\nSPHERICAL")
+    datTree.spherical_region(config)
+    dat_mX_VRls = datTree.np('X_m')[datTree.V_SRls_mask]
+    dat_mX_VRhs = datTree.np('X_m')[datTree.V_SRhs_mask]
+    dat_mX_SRls = datTree.np('X_m')[datTree.A_SRls_mask]
+else:
+    raise AttributeError("No mass region definition!")
+
+### ------------------------------------------------------------------------------------
+## train bdt and predict data SR weights
 
 print(".. predicting weights in validation region")
 VR_weights, SR_weights = datTree.bdt_process(region_type, config)
 
-# with or without BDT
-# ks_noBDT = datTree.kstest(BDT=False)
-# ks_BDT = datTree.kstest() # may need to add a normalization factor eventually - to be studied
-
-# df = pd.concat([ks_noBDT, ks_BDT], axis=0)
-# df.index = ['TF','BDT']
-# print(df.T)
-
-sys.exit()
+if args.testing: sys.exit()
 
 fig, ax = plt.subplots()
 n_estimate, _ = Hist(dat_mX_VRls, weights=VR_weights, bins=mBins, ax=ax, label='Estimation')
@@ -168,20 +183,19 @@ ax.set_ylabel("Events")
 ax.set_title("BDT Estimation of Data Yield in Validation Region")
 fig.savefig(f"combine/{outputFile}_validation.pdf", bbox_inches='tight')
 
-### ------------------------------------------------------------------------------------
-## signal region estimate
-
-n_sig_SRhs, _ = np.histogram(sig_mX_SRhs, bins=mBins)
-n_sig_SRhs = n_sig_SRhs * sigTree.scale
-
 n_dat_SRhs_estimate, e = Hist(dat_mX_SRls, weights=SR_weights, bins=mBins, ax=ax, label='Estimation')
 
-print("N(data,SR,est) =",n_dat_SRhs_estimate.sum())
+print("N(data,SR,est) =",int(n_dat_SRhs_estimate.sum()))
+
+### ------------------------------------------------------------------------------------
+## signal region estimate
 
 canvas = ROOT.TCanvas('c1','c1', 600, 600)
 canvas.SetFrameLineWidth(3)
 
 h_dat = ROOT.TH1D("h_dat",";m_{X} [GeV];Events",len(n_dat_SRhs_estimate),array('d',list(e)))
+
+# for 
 h_sig = ROOT.TH1D("h_sig",";m_{X} [GeV];Events",len(n_sig_SRhs),array('d',list(e)))
 
 for i,(bin_sig,bin_dat) in enumerate(zip(n_sig_SRhs, n_dat_SRhs_estimate)):
@@ -217,6 +231,8 @@ print()
 print("PLEASE RUN THE FOLLOWING COMMAND:")
 print("---------------------------------")
 print("ssh srosenzw@cmslpc-sl7.fnal.gov 'cd /uscms/home/srosenzw/nobackup/workarea/higgs/sixb_analysis/CMSSW_10_2_18/src/HiggsAnalysis/CombinedLimit/; source env_standalone.sh; make -j 8; make; python3 generateDataCards.py'")
+print("---------------------------------")
+print("END PROCESS\n")
 # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 # output, error = process.communicate()
 # bashCommand = "runcombine"
