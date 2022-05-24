61bd247e3d7ff57de1c4a9c6b00f1274b69909ee
Suzanne <srosenzweig09@gmail.com>
2022-05-24 10:00:13 -0400

    Tidying

--- scripts/runCombine.sh
+++ scripts/runCombine.sh
@@ -14,20 +14,22 @@ while true; do
     --)
       break;;
      *)
-      printf "Unknown option %s\n" "$1";;
+      printf "Unknown option %s\n" "$0";;
     #   exit 1;;
   esac
 done
 
-if ${rect}; then
-    echo "[BASH] Running rectangular skimRegions"
-    python scripts/skimRegions.py --cfg config/rectConfig.cfg --rectangular || exit 
-fi
+# if ${rect}; then
+#     echo "[BASH] Running rectangular skimRegions"
+#     python scripts/skimRegions.py --cfg config/rectConfig.cfg --rectangular --all-signal || exit 
+#     echo "[BASH] Running HiggsCombine"
+#     ssh srosenzw@cmslpc-sl7.fnal.gov 'cd /uscms/home/srosenzw/nobackup/workarea/higgs/sixb_analysis/CMSSW_10_2_18/src/HiggsAnalysis/CombinedLimit/; source env_standalone.sh > /dev/null; make -j 8 > /dev/null; make > /dev/null; python3 runCombine.py --rect'
+# fi
 
 if ${sphere}; then
+    echo "[BASH] Generating new spherical config file"
+    python scripts/sphere_scanner.py --rIn $2 --rOut $3
     echo "[BASH] Running spherical skimRegions"
-    python scripts/skimRegions.py --cfg config/rectConfig.cfg --spherical || exit 
+    python scripts/skimRegions.py --cfg config/sphereConfig_new.cfg --spherical --all-signal || exit 
+    ssh srosenzw@cmslpc-sl7.fnal.gov 'cd /uscms/home/srosenzw/nobackup/workarea/higgs/sixb_analysis/CMSSW_10_2_18/src/HiggsAnalysis/CombinedLimit/; source env_standalone.sh > /dev/null; make -j 8 > /dev/null; make > /dev/null; python3 runCombine.py --sphere --tag '${2}_${3}
 fi
-
-echo "[BASH] Running HiggsCombine"
-ssh srosenzw@cmslpc-sl7.fnal.gov 'cd /uscms/home/srosenzw/nobackup/workarea/higgs/sixb_analysis/CMSSW_10_2_18/src/HiggsAnalysis/CombinedLimit/; source env_standalone.sh > /dev/null; make -j 8 > /dev/null; make > /dev/null; python3 generateDataCards.py'
