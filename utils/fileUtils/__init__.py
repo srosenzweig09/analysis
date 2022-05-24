ef20bc71d1e9214b1fcf108d4341066663167872
Suzanne <srosenzweig09@gmail.com>
2022-05-24 09:56:02 -0400

    Updated file accessibility

--- utils/fileUtils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sr import FileLocations, mx_my_masses
-
-combineDir = '/uscms/home/srosenzw/nobackup/workarea/higgs/sixb_analysis/CMSSW_10_2_18/src/HiggsAnalysis/CombinedLimit/'
-
-files = FileLocations()
