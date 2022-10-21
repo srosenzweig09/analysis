cea9d72d2f80a1c54a17629860d4e9fe2b7110f0
Suzanne <srosenzweig09@gmail.com>
2022-10-21 17:19:28 -0400

    Added versatility with construction from kinematic dict

--- utils/analysis/particle.py
+++ utils/analysis/particle.py
@@ -3,17 +3,34 @@ import numpy as np
 np.seterr(all="ignore")
 import vector
 vector.register_awkward()
+import sys
 
 class Particle():
-    def __init__(self, tree=None, particle_name=None, particle=None):
+    def __init__(self, tree=None, particle_name=None, particle=None, kin_dict=None):
         """A class much like the TLorentzVector objects in the vector module but with more customizability for my analysis
         """
 
         if tree is not None and particle_name is not None:
             self.initialize_from_tree(tree, particle_name)
         elif particle is not None:
-            self. initialize_from_particle(particle)
+            self.initialize_from_particle(particle)
+        elif kin_dict is not None:
+            self.initialize_from_kinematics(
+                kin_dict['pt'],
+                kin_dict['eta'],
+                kin_dict['phi'],
+                kin_dict['m']
+            )
         self.P4 = self.get_vector()
+        
+        self.theta = self.P4.theta
+        self.costheta = np.cos(self.theta)
+
+    def initialize_from_kinematics(self, pt, eta, phi, m):
+        self.pt = pt
+        self.eta = eta
+        self.phi = phi
+        self.m = m
 
     def initialize_from_tree(self, tree, particle_name):
         self.pt = tree.get(particle_name + '_pt')
@@ -21,16 +38,12 @@ class Particle():
         self.eta = tree.get(particle_name + '_eta')
         self.phi = tree.get(particle_name + '_phi')
         self.m = tree.get(particle_name + '_m')
-        self.theta = ak.from_numpy(2*np.arctan(np.exp(-self.eta.to_numpy())))
-        self.cosTheta = np.cos(self.theta)
 
     def initialize_from_particle(self, particle):
         self.pt = particle.pt
         self.eta = particle.eta
         self.phi = particle.phi
         self.m = particle.m
-        self.theta = ak.from_numpy(2*np.arctan(np.exp(-self.eta.to_numpy())))
-        self.cosTheta = np.cos(self.theta)
 
     def get_vector(self):
         p4 = vector.obj(
