ebfa690cb3aa7c752983abbe0b123671a405ab0f
Suzanne <srosenzweig09@gmail.com>
2022-12-14 11:02:54 -0500

    b tag working points 2018

--- utils/cutConfig.py
+++ utils/cutConfig.py
@@ -2,5 +2,13 @@ nullWP,looseWP,mediumWP,tightWP = 0,1,2,3
 wplabels = ["null","loose","medium","tight"]
 wpchars = ["N","L","M","T"]
 
+btagWP = {}
+btagWP[2018] = {
+    'Fail' : 0,
+    'Loose' : 0.0490,
+    'Medium' : 0.2783,
+    'Tight' : 0.7100
+    }
+
 jet_btagWP = [0,0.0490,0.2783,0.7100]
 jet_ptWP = [20,40,60,80]
