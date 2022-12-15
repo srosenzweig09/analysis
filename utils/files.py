c7ed1020729d37c166eff5b7e7228b9bf995034a
Suzanne <srosenzweig09@gmail.com>
2022-12-15 10:16:53 -0500

    Added new mass points, updated locations

--- utils/files.py
+++ utils/files.py
@@ -13,26 +13,32 @@ mx_my_masses = [
     [ 900,300], [ 900,400], [ 900,500], [ 900,600], [ 900,700],
     [1000,300], [1000,400], [1000,500], [1000,600], [1000,700], [1000,800],
     [1100,300], [1100,400], [1100,500], [1100,600], [1100,700], [1100,800], [1100,900],
-    [1200,300], [1200,400], [1200,500], [1200,600], [1200,700], [1200,800], [1200,900],
+    [1200,300], [1200,400], [1200,500], [1200,600], [1200,700], [1200,800], [1200,900], [1200,1000]
     ]
 
 fnal_root = "root://cmseos.fnal.gov/"
 base = '/eos/uscms/store/user/srosenzw/sixb/ntuples'
-current_base = f"{base}/Summer2018UL/bias/"
+current_base = f"{base}/Summer2018UL/"
 data_path = 'JetHT_Data_UL/JetHT_Run2018_full/ntuple.root'
 config = 'config/sphereConfig.cfg'
 
-def get_NMSSM(mx, my, run='Summer2018UL', jets='bias'):
-   return f"{base}/{run}/{jets}/NMSSM/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
-
-def get_presel(mx=700, my=400, run='Summer2018UL'):
-   return f"{base}/{run}/NMSSM_cutflow_studies/presel/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
-
-def get_trigger(mx=700, my=400, run='Summer2018UL'):
-   return f"{base}/{run}/NMSSM_trigger/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
+def get_NMSSM(mx=700, my=400, run='Summer2018UL', jets='btag_pt/', cut=None):
+   if cut is not None: jets = ''
+   file_dict = {
+      'presel' : 'cutflow_studies/presel/NMSSM',
+      'trigger' : 'cutflow_studies/trigger/NMSSM',
+      'presel' : 'cutflow_studies/presel/NMSSM',
+   }
+   return f"{base}/{run}/{jets}{file_dict.get(cut, '')}/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
 
 def get_nocuts(mx=700, my=400, run='Summer2018UL', study='nocuts'):
-   return f"{base}/{run}/NMSSM_nocuts/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
+   return f"{base}/{run}/cutflow_studies/nocuts/NMSSM/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
+
+def get_trigger(mx=700, my=400, run='Summer2018UL'):
+   return f"{base}/{run}/cutflow_studies/trigger/NMSSM/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
+
+def get_presel(mx=700, my=400, run='Summer2018UL'):
+   return f"{base}/{run}/cutflow_studies/presel/NMSSM/NMSSM_XYH_YToHH_6b_MX_{mx}_MY_{my}/ntuple.root"
 
 def get_data(run='Summer2018UL', jets='bias', btag=False):
    if btag: jets = jets + '_maxbtag'
@@ -82,6 +88,20 @@ def get_signal_list(run='Summer2018UL', jets='bias'):
 #       mxmy_loc  = f'{self.base}/NMSSM/{mxmy_name}/ntuple.root'
 #       return getattr(self, mxmy_name)
 
+def get_qcd_enriched(selection, run='Summer2018UL'):
+   QCD_bEn_Ht_100to200   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT100to200_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_200to300   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT200to300_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_300to500   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT300to500_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_500to700   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT500to700_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_700to1000  = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_1000to1500 = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_1500to2000 = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT1500to2000_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+   QCD_bEn_Ht_2000toInf  = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
+
+   QCD_bEn_List = [QCD_bEn_Ht_100to200,QCD_bEn_Ht_200to300,QCD_bEn_Ht_300to500, QCD_bEn_Ht_500to700, QCD_bEn_Ht_700to1000, QCD_bEn_Ht_1000to1500,QCD_bEn_Ht_1500to2000,QCD_bEn_Ht_2000toInf]
+
+   return QCD_bEn_List
+
 def get_qcd_list(selection, run='Summer2018UL'):
    QCD_bEn_Ht_100to200   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT100to200_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
    QCD_bEn_Ht_200to300   = f"{base}/{run}/{selection}/QCD/QCD_bEnriched_HT200to300_TuneCP5_13TeV-madgraph-pythia8/ntuple.root"
@@ -109,10 +129,11 @@ def get_qcd_list(selection, run='Summer2018UL'):
    return QCD_B_List
 
 def get_ttbar(selection, run='Summer2018UL'):
-   TTJets = f"{base}/{run}/{selection}/TTJets/TTJets/ntuple.root"
+   TTJets = f"{base}/{run}/{selection}/TTJets/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/ntuple_1.root"
    return TTJets
 
-def get_qcd_ttbar(selection, run='Summer2018UL'):
+def get_qcd_ttbar(selection, run='Summer2018UL', enriched=True):
+   if enriched: return get_qcd_enriched(selection, run) + [get_ttbar(selection, run)]
    return get_qcd_list(selection, run) + [get_ttbar(selection, run)]
 
 
