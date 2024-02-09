e377cbf15c4f32c0ac81064bc6a26008294bcdf4
Suzanne <srosenzweig09@gmail.com>
2024-02-09 13:01:43 +0100

    Moved mass_dict initialization into init_new_model (not needed for old_model)

--- utils/analysis/feyn.py
+++ utils/analysis/feyn.py
@@ -2,8 +2,7 @@ import json, uproot
 import numpy as np
 import awkward as ak
 
-model_name = '20230731_7d266883bbfb88fe4e226783a7d1c9db_ranger_lr0.0047_batch2000_withbkg'
-old_model_path = f'/eos/uscms/store/user/srosenzw/weaver/models/exp_sixb_official/feynnet_ranker_6b/{model_name}/predict_output'
+old_model_path = f'/eos/uscms/store/user/srosenzw/weaver/models/exp_sixb_official/feynnet_ranker_6b/20230731_7d266883bbfb88fe4e226783a7d1c9db_ranger_lr0.0047_batch2000_withbkg/predict_output'
 
 new_model_path = "/eos/uscms/store/user/srosenzw/weaver/cmsuf/data/store/user/srosenzw/lightning/models/feynnet_lightning/X_YH_3H_6b/x3h/lightning_logs/version_23183119/predict/"
 
@@ -15,14 +14,13 @@ def getMassDict(year):
 class Model():
 
     def __init__(self, which, sixb):
-        mass_dict = getMassDict(sixb.year)
-
-        if which == 'new': self.init_new_model(sixb, mass_dict)
+        if which == 'new': self.init_new_model(sixb)
         elif which == 'old': self.init_old_model(sixb)
         else: raise ValueError(f"Model type '{which}' not recognized")
 
     def init_new_model(self, sixb, mass_dict):
         print("------ new -------")
+        mass_dict = getMassDict(sixb.year)
         hash = mass_dict[sixb.filename]
         model_path = f"{new_model_path}/{sixb.year}/{hash}.root"
         with uproot.open(model_path) as f:
