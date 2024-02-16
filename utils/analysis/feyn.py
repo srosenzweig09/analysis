69ea9b6799960d4ae2f3bb91b1e53f3c4eefa9fc
Suzanne Rosenzweig <srosenzweig09@gmail.com>
2024-02-16 18:36:08 +0100

    sixb -> tree for general use

--- utils/analysis/feyn.py
+++ utils/analysis/feyn.py
@@ -4,10 +4,11 @@ import awkward as ak
 from utils.analysis.particle import Particle, Higgs, Y
 
 old_model_name = '20230731_7d266883bbfb88fe4e226783a7d1c9db_ranger_lr0.0047_batch2000_withbkg'
-old_model_path = f'/eos/uscms/store/user/srosenzw/weaver/models/exp_sixb_official/feynnet_ranker_6b/{old_model_name}/predict_output'
+old_model_path = f'/eos/uscms/store/user/srosenzw/weaver/models/exp_tree_official/feynnet_ranker_6b/{old_model_name}/predict_output'
 
 new_model_name = 'version_23183119'
 # new_model_path = f"/eos/uscms/store/user/srosenzw/weaver/cmsuf/data/store/user/srosenzw/lightning/models/feynnet_lightning/X_YH_3H_6b/x3h/lightning_logs/{new_model_name}/predict/"
+new_model_path = f"/cmsuf/data/store/user/srosenzw/lightning/models/feynnet_lightning/X_YH_3H_6b/x3h/lightning_logs/{new_model_name}/predict/"
 new_model_path = f"/cmsuf/data/store/user/srosenzw/lightning/models/feynnet_lightning/X_YH_3H_6b/x3h/lightning_logs/{new_model_name}/predict/"
 
 def getMassDict(year):
@@ -17,19 +18,19 @@ def getMassDict(year):
 
 class Model():
 
-    def __init__(self, which, sixb):
-        if which == 'new': self.init_new_model(sixb)
-        elif which == 'old': self.init_old_model(sixb)
+    def __init__(self, which, tree):
+        if which == 'new': self.init_new_model(tree)
+        elif which == 'old': self.init_old_model(tree)
         else: raise ValueError(f"Model type '{which}' not recognized")
 
-        self.init_particles(sixb)
-        self.copy_attributes(sixb)
+        self.init_particles(tree)
+        self.copy_attributes(tree)
 
-    def init_new_model(self, sixb):
+    def init_new_model(self, tree):
         print(".. initializing new FeynNet model")
-        mass_dict = getMassDict(sixb.year)
-        model_path = mass_dict[sixb.filepath]
-        # model_path = f"{new_model_path}/{sixb.year}/{hash}.root"
+        mass_dict = getMassDict(tree.year_long)
+        model_path = mass_dict[tree.filepath]
+        # model_path = f"{new_model_path}/{tree.year}/{hash}.root"
         with uproot.open(model_path) as f:
             t = f['Events']
             # print(t['sorted_rank'].array())
@@ -38,9 +39,9 @@ class Model():
         self.model_name = new_model_name
         # print(print(combos))
 
-    def init_old_model(self, sixb):
+    def init_old_model(self, tree):
         print(".. initializing old FeynNet model")
-        model_path = f"{old_model_path}/{sixb.year}/{sixb.filename}.root"
+        model_path = f"{old_model_path}/{tree.year}/{tree.filename}.root"
         with uproot.open(model_path) as f:
             f = f['Events']
             maxcomb = f['max_comb'].array(library='np')
@@ -49,19 +50,19 @@ class Model():
         self.combos = ak.from_regular(combos)
         self.model_name = old_model_name
 
-    def init_particles(self, sixb):
-        btag_mask = ak.argsort(sixb.jet_btag, axis=1, ascending=False) < 6
+    def init_particles(self, tree):
+        btag_mask = ak.argsort(tree.jet_btag, axis=1, ascending=False) < 6
 
-        pt = sixb.jet_ptRegressed[btag_mask][self.combos]
-        phi = sixb.jet_phi[btag_mask][self.combos]
-        eta = sixb.jet_eta[btag_mask][self.combos]
-        m = sixb.jet_mRegressed[btag_mask][self.combos]
-        btag = sixb.jet_btag[btag_mask][self.combos]
-        sig_id = sixb.jet_signalId[btag_mask][self.combos]
-        h_id = (sixb.jet_signalId[btag_mask][self.combos] + 2) // 2
+        pt = tree.jet_ptRegressed[btag_mask][self.combos]
+        phi = tree.jet_phi[btag_mask][self.combos]
+        eta = tree.jet_eta[btag_mask][self.combos]
+        m = tree.jet_mRegressed[btag_mask][self.combos]
+        btag = tree.jet_btag[btag_mask][self.combos]
+        sig_id = tree.jet_signalId[btag_mask][self.combos]
+        h_id = (tree.jet_signalId[btag_mask][self.combos] + 2) // 2
 
         self.btag_avg = ak.mean(btag, axis=1)
-        setattr(sixb, 'btag_avg', self.btag_avg)
+        setattr(tree, 'btag_avg', self.btag_avg)
 
         sample_particles = []
         for j in range(6):
@@ -85,7 +86,7 @@ class Model():
         H2_b2 = {'pt':sample_particles[5].pt,'eta':sample_particles[5].eta,'phi':sample_particles[5].phi,'m':sample_particles[5].m,'btag':sample_particles[5].btag,'sig_id':sample_particles[5].sig_id,'h_id':sample_particles[5].h_id}
 
         self.HX = Higgs(HX_b1, HX_b2)
-        setattr(sixb, 'HX', self.HX)
+        setattr(tree, 'HX', self.HX)
 
         H1 = Higgs(H1_b1, H1_b2)
         H2 = Higgs(H2_b1, H2_b2)
@@ -95,17 +96,17 @@ class Model():
         assert ak.all(H2.b1.pt >= H2.b2.pt)
 
         self.Y = Y(H1, H2)
-        setattr(sixb, 'Y', self.Y)
+        setattr(tree, 'Y', self.Y)
 
         self.H1 = self.Y.H1
         self.H2 = self.Y.H2
-        setattr(sixb, 'H1', self.H1)
-        setattr(sixb, 'H2', self.H2)
+        setattr(tree, 'H1', self.H1)
+        setattr(tree, 'H2', self.H2)
 
         assert ak.all(self.H1.pt >= self.H2.pt)
 
         self.X = self.HX + self.H1 + self.H2
-        setattr(sixb, 'X', self.X)
+        setattr(tree, 'X', self.X)
 
         self.higgs_bjet_sig_id = np.column_stack((
             self.HX.b1.sig_id.to_numpy(),
